84f3af6a84c633459fe3e89c30df8ccfc715b39c
hovinh <hovinh39@gmail.com>
2017-10-21 12:28:16 +0800

    solver.py: implement ethnic constraint

--- source/solver.py
+++ source/solver.py
@@ -63,7 +63,7 @@ class IntegerProgrammingSolver(Solver):
         for flat_index in range(self._numb_total_flats):
             flat_positions = [i for i in range(flat_index, self._numb_agents * self._numb_total_flats,
                                                  self._numb_total_flats)]
-            A_flat[flat_index][flat_positions] = np.ones([1, self._numb_agents])     
+            A_flat[flat_index][flat_positions] = np.ones([1, self._numb_agents])  
             A.append(self.convert_A_to_dict(X, A_flat[flat_index]))
                         
         for i in range(self._numb_agents + self._numb_total_flats):
@@ -71,21 +71,45 @@ class IntegerProgrammingSolver(Solver):
 
         # for each block, there is a limited capacity for each ethnicity
         if (has_ethnicity == True):
-            numb_ethnicities = len(self._ethnic_capacity_per_block[0])
+            numb_ethnicities = len(self._ethnicity_list)
+            A_ethnic = np.zeros([self._numb_blocks * numb_ethnicities, self._numb_agents * self._numb_total_flats])
             
             for block_index in range(self._numb_blocks):
                 
-                for ethnicity_index in range(numb_ethnicities):
+                for ethnic_index in range(numb_ethnicities):
+                    # determine positions of flats of current block
+                    row_index = block_index * numb_ethnicities + ethnic_index
+                    numb_flats_prev_blocks, numb_flats_cur_blocks, numb_flats_next_blocks = 0, 0, 0
                     
-                    row_index = block_index * self._numb_blocks + ethnicity_index
-                    '''TODO HERE'''
+                    for i in range(block_index):
+                        numb_flats_prev_blocks += self._numb_flats_per_block[i]
+
+                    for i in range(block_index+1, self._numb_blocks):
+                        numb_flats_next_blocks += self._numb_flats_per_block[i]
+
+                    numb_flats_cur_blocks = self._numb_flats_per_block[block_index]
+                
+                    prev_blocks_positions = [0 for i in range(numb_flats_prev_blocks)]
+                    cur_blocks_positions = [1 for i in range(numb_flats_cur_blocks)]
+                    next_blocks_positions = [0 for i in range(numb_flats_next_blocks)]
+
+                    block_positions = prev_blocks_positions + cur_blocks_positions + next_blocks_positions
+                    block_positions = np.array(block_positions * self._numb_agents)
                     
+                    # among chosen positions, find agents having interested ethnicity
+                    ethnicity = self._ethnicity_list[ethnic_index]
+                    ethnicity_considered = np.array([ethnicity] * self._numb_agents * self._numb_total_flats)
+                    ethnicity_positions = ethnicity_considered == self._ethnic_agents 
+                    ethnicity_positions = np.logical_and(ethnicity_positions, block_positions)
+
+                    A_ethnic[row_index] = ethnicity_positions
+                    A.append(self.convert_A_to_dict(X, A_ethnic[row_index]))                    
                     
             for i in range(self._numb_agents + self._numb_total_flats,
                            self._numb_agents + self._numb_total_flats 
                            + self._numb_blocks * numb_ethnicities):
-                ethnic_index = i % (self._numb_agents + self._numb_total_flats) 
-                block_index = (i - (self._numb_agents + self._numb_total_flats)) / numb_ethnicities
+                ethnic_index = (i - (self._numb_agents + self._numb_total_flats)) % numb_ethnicities 
+                block_index = int((i - (self._numb_agents + self._numb_total_flats)) / numb_ethnicities)
                 prob += lpSum(A[i][j] * x_vars[j] for j in X) <= self._ethnic_capacity_per_block[block_index][ethnic_index]
                 
         GLPK().solve(prob)
@@ -102,22 +126,32 @@ class IntegerProgrammingSolver(Solver):
             A_temp[x_i] = indicator
         return A_temp
     
-    def add_ethnicity(self, utility, read_from_file = False, ethnic_capacity_per_block = None):
+    def add_ethnicity(self, utility, read_from_file = False, file_name = 'test_data.txt',
+                      ethnicity_list = None, ethnic_capacity_per_block = None):
+        self._ethnicity_list = None
         self._ethnic_capacity_per_block = None
+        
         if (read_from_file == False):
+            self._ethnicity_list = ethnicity_list
             self._ethnic_capacity_per_block = ethnic_capacity_per_block
         else:
-            self._ethnic_capacity_per_block = self.read_ethnic_capacity_per_block()
+            self._ethnicity_list, self._ethnic_capacity_per_block = self.read_ethnic_capacity_per_block(file_name)
         
         self._ethnic_agents = self.read_utility_ethnic_agents(utility._ethnic_agents)
 
-    def read_ethnic_capacity_per_block(self):
+    def read_ethnic_capacity_per_block(self, file_name):
+        ethnicity_list = []
         ethnic_capacity_per_block = []
-        with open('ethnic_capacity_per_block.txt', 'r') as file:
+        with open(file_name, 'r') as file:
+            is_first_line = True
             for line in file:
                 line = line.strip()
-                ethnic_capacity_per_block.append([int(i) for i in line.split(',')])
-        return ethnic_capacity_per_block
+                if (is_first_line == True):
+                    ethnicity_list = [i for i in line.split(',')]
+                    is_first_line = False
+                else:
+                    ethnic_capacity_per_block.append([int(i) for i in line.split(',')])
+        return ethnicity_list, ethnic_capacity_per_block
         
     def read_utility_ethnic_agents(self, utility_ethnic_agents):
         ethnic_agents = np.full((1, self._numb_total_flats), 'Q')
@@ -125,7 +159,7 @@ class IntegerProgrammingSolver(Solver):
             agent_duplications = np.full((1, self._numb_total_flats), ethnic_agent)
             ethnic_agents = np.concatenate((ethnic_agents, agent_duplications), axis = 0)
         ethnic_agents = np.ndarray.flatten(ethnic_agents[1:])
-        return np.array(ethnic_agents)
+        return ethnic_agents
         
 def test1():
     np.random.seed(0)
@@ -162,9 +196,9 @@ def test2():
         
 def test3():
     np.random.seed(0)
-    numb_agents = 10
+    numb_agents = 50
     numb_blocks = 5
-    numb_flats_per_block = [10, 10, 10, 10, 10]
+    numb_flats_per_block = [10, 10, 10, 10, 5]
     
     utility = RandomUtility(numb_agents, numb_blocks, numb_flats_per_block)
     utility.generate()
@@ -175,6 +209,8 @@ def test3():
     
     print ('Status:', LpStatus[solver._prob.status])    
     print ('Optimal value:', value(solver._prob.objective))
+    x_vars = [v.varValue for v in solver._prob.variables()]
+    print ('Total x:', np.sum(x_vars))
 
 def test4():
     np.random.seed(0)
@@ -197,9 +233,10 @@ def test4():
         indian_capacity = int(numb_flats_per_block[i] * ethnic_capacity_ratio_per_block[i]['INDIAN'])
         ethnic_capacity_per_block.append([chinese_capacity, malay_capacity, indian_capacity])
         
-    solver.add_ethnicity(utility, False, ethnic_capacity_per_block)
+    solver.add_ethnicity(utility, False, ethnicity_list = ['C', 'M', 'I'], ethnic_capacity_per_block = ethnic_capacity_per_block)
     print (solver._ethnic_capacity_per_block)
     print (solver._ethnic_agents.shape)
+    print (solver._ethnicity_list)
     solver.calculate()
     
     print ('Status:', LpStatus[solver._prob.status])    
@@ -221,11 +258,43 @@ def test5():
     solver.add_ethnicity(utility, True)
     print (solver._ethnic_capacity_per_block)
     print (solver._ethnic_agents.shape)
+    print (solver._ethnicity_list)
+
+def test6():
+    np.random.seed(10)
+    numb_agents = 50
+    numb_blocks = 5
+    numb_flats_per_block = [10, 10, 10, 10, 10]
+    
+    utility = RandomUtility(numb_agents, numb_blocks, numb_flats_per_block)
+    utility.add_ethnicity()
+    utility.generate()
+
+    solver = IntegerProgrammingSolver(numb_agents, numb_blocks, numb_flats_per_block,
+                                 utility)
+    solver.calculate()    
+    print ('Status:', LpStatus[solver._prob.status])    
+    print ('Optimal value:', value(solver._prob.objective))
+    
+    
+    ethnic_capacity_ratio_per_block = [{'CHINESE': 0.847, 'MALAY': 0.154, 'INDIAN': 0.088}
+                                 for i in range(numb_blocks)]
+    ethnic_capacity_per_block = [[8, 2, 2] for i in range(numb_blocks)]
+        
+    solver.add_ethnicity(utility, False, ethnicity_list=['C', 'M', 'I'], ethnic_capacity_per_block=ethnic_capacity_per_block)
+    print (solver._ethnic_capacity_per_block)
+    print (solver._ethnic_agents.shape)
+    print (solver._ethnicity_list)
+    
+    solver.calculate(has_ethnicity = True)
+    print ('Status:', LpStatus[solver._prob.status])    
+    print ('Optimal value:', value(solver._prob.objective))
     
 if __name__ == '__main__':
     ''' Test suite for IntegerProgrammingSolver '''
     #test1()
     #test2()
     #test3() # costly, check for difference between # agents and # flats
-    test4()
-    test5()   
+    #test4()
+    #test5()   
+    test6()
